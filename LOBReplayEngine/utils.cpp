b31b6a1c69012b0f47cdb537de5f3a0046d37ead
arya <aryasomu18@gmail.com>
2025-05-23 12:38:59 -0700

    chore: implement toLower function

---
+++ LOBReplayEngine/utils.cpp
@@ -0,0 +1,8 @@
+#include "utils.hpp"
+#include <algorithm>
+
+std::string toLower(const std::string& s) {
+    std::string out = s;
+    std::transform(out.begin(), out.end(), out.begin(), ::tolower);
+    return out;
+}
