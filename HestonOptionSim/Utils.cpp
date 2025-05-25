4ed3cbec918a90a568383b6f3f2543274a095960
arya <aryasomu18@gmail.com>
2025-05-24 22:29:40 -0700

    chore: implement randn + norm math

---
+++ HestonOptionSim/Utils.cpp
@@ -0,0 +1,18 @@
+#include "Utils.hpp"
+#include <random>
+#include <cmath>
+
+double norm_cdf(double x) {
+    return 0.5 * std::erfc(-x / std::sqrt(2));
+}
+
+double norm_pdf(double x) {
+    static const double inv_sqrt_2pi = 0.3989422804014327;
+    return inv_sqrt_2pi * std::exp(-0.5 * x * x);
+}
+
+double randn() {
+    static std::mt19937 rng(std::random_device{}());
+    static std::normal_distribution<> dist(0.0, 1.0);
+    return dist(rng);
+}
